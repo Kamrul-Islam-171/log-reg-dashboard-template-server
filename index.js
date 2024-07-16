2f36102c765fc2f70be39784dda689cbdbfd5f56
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-07-16 12:07:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+// var jwt = require('jsonwebtoken');
+
+
+
+
+
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}))
+
+
+app.get('/', (req, res) => {
+  res.send('MFS!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
